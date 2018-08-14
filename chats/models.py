df46fe2bb92a0a746c21e81814ec728da91a1134
loay elzobaidy <loayelzobeidy@gmail.com>
2018-08-15 01:06:15 +0200

    all questions added but there is still a bug

--- chats/models.py
+++ chats/models.py
@@ -3,13 +3,20 @@ from __future__ import unicode_literals
 
 from django.db import models
 # Create your models here.
+class Skill(models.Model):
+    skill =  models.CharField(max_length=200, primary_key=True)
+    def __str__(self):
+        return self.skill
+
+    class Meta:
+        ordering = ('skill',)
 class User(models.Model):
     email = models.CharField(max_length=200, primary_key=True)
     name = models.CharField(max_length=50)
-    skills = models.ManyToManyField("Skill")
-    employed = models.BooleanField()
+    skills = models.ManyToManyField(Skill)
+    employed = models.BooleanField(default=False)
     job_title = models.CharField(max_length=200)
-    years_of_experience = models.IntegerField()
+    years_of_experience = models.IntegerField(default=0)
     seeking_titles = models.ManyToManyField("Title") 
        
 class Question(models.Model):
@@ -18,8 +25,6 @@ class Question(models.Model):
           # other stuff
     question = models.CharField(max_length=300)
 
-class Skill(models.Model):
-    skill =  models.CharField(max_length=200, primary_key=True)
 class Title(models.Model):
     title =  models.CharField(max_length=200, primary_key=True)
     
--- chats/views.py
+++ chats/views.py
@@ -2,7 +2,7 @@
 
 import json
 from django.shortcuts import render
-from chats.models import Question, User
+from chats.models import Question, User,Skill,Title
 # Create your views here.
 from django.http import HttpResponse
 
@@ -28,11 +28,12 @@ def get_questions(request):
     # return HttpResponse(json_stuff, content_type ="application/json")
 
 def chat(request):
-    if request.method != 'POST':
+    index  = int(request.COOKIES.get('index', '0') )    
+    if request.method != 'POST' and not index==0:
         print('exceptionn')
-        return
+        return  HttpResponse(json.dumps({"message":"Should be a post request"}), content_type ="application/json")
     dictionaries = [ Question.as_dict() for Question in Question.objects.all()]
-    index  = int(request.COOKIES.get('index', '0') )
+    print(dictionaries)
     index  = index % len(dictionaries)
     print("index===>>",index)    
     body_unicode = request.body
@@ -40,16 +41,52 @@ def chat(request):
     email = body['email']
     name = body['name']
     print(User.objects.filter(email=email),"userssss")
-    if(index==1):
+    if(index==2):
         skills = body['skills']
-        u = User(email=email,name=name,employed=False,years_of_experience=0)
+        u = User(email=email)
+        u.employed = False
+        u.years_of_experience = 0
+        for skill in skills:
+           sk = Skill(skill=skill)
+           sk.save()
+           u.save()
+           u.skills.add(sk)
+        print(u,"usersss==>>>")
         u.save()
+    if(index==3):
+        employed = body['employed']
+        u = User(email=email)
+        u.years_of_experience = 0
+        u.employed = employed
+        u.save()
+    print(User.objects.filter(email=email),"userssss")    
     question = dictionaries[index]
+    if(index==6):
+        titles = body['titles']
+        u = User(email=email)
+        for title in titles:
+           sk = Title(title=title)
+           sk.save()
+           u.save()
+           u.skills.add(sk)
+        print(u,"usersss==>>>")
+    if(index==5):
+        years_of_experience = body['experience']
+        u = User(email=email)
+        u.years_of_experience = years_of_experience
+        u.save()
+    if(index==4):
+        job_title = body['current_title']
+        u = User(email=email)
+        u.job_title = job_title
+        u.save()
+        print(u,"usersss==>>>")
+    if(index==4 and u.employed==False):
+        index +=2
     if(question[0]==','):
         question = 'Hi '+name+' '+dictionaries[index]
     if(question[len(question)-1]==','):
         question = dictionaries[index]+' '+name
-    
     response  = HttpResponse(json.dumps(question), content_type ="application/json")
     print("question",dictionaries[index])
     response.set_cookie('index', index + 1 )
